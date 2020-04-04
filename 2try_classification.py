7480aa5d52e266a476dfb6125c49c1847afb0eb4
Marco De Pasquale <depasquale.marco@gmail.com>
2020-04-04 19:49:18 +0200

    tried all models

--- 2try_classification.py
+++ 2try_classification.py
@@ -124,7 +124,7 @@ for ((X_set, y_set), label) in zip(set_list, set_label_list) :
     X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                          np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
     plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-                 alpha = 0.75, cmap = ListedColormap(('red', 'green', 'orange')))
+                 alpha = 0.75, cmap = ListedColormap(('red', 'green', 'yellow')))
     plt.xlim(X1.min(), X1.max())
     plt.ylim(X2.min(), X2.max())
     for i, j in enumerate(np.unique(y_set)):
